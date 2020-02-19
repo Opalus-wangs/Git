fbbfec341e3ddf70a07ea6e683308d80bcb01fae
Opalus-wangs <785554487@qq.com>
2020-02-19 20:57:28 +0800

    cifar10-learn1

--- local2.py
+++ local2.py
@@ -24,7 +24,6 @@ with open(os.path.join(CIFAR_DIR,"data_batch_1"),'rb') as f:
 #labels:标签
 #[6 9]分别代表第7类和第10类
 
-
 image_arr = data[b'data'][100]
 image_arr = image_arr.reshape((3, 32, 32)) #3表示3个维度，32 32分别表示图片大小32*32
 image_arr = image_arr.transpose((1, 2,0))#图片通道显示顺序为 32 32 3 所以需要交换通道
