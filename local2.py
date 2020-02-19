7cd8b43b90afa21f3b563ad8d7db579139dc9f76
Opalus-wangs <785554487@qq.com>
2020-02-19 20:43:12 +0800

    cifar10-learn1

--- local2.py
+++ local2.py
@@ -1,8 +1,37 @@
+import _pickle as cPickle
+import numpy as np
+import os
 
-# -*- encoding: utf-8 -*-
-from __future__ import print_function, unicode_literals
-import json
+CIFAR_DIR = ".\cifar-10-batches-py"
+print(os.listdir(CIFAR_DIR))
 
-int (a=5)
-int (b=3)
-print("hello World")
+with open(os.path.join(CIFAR_DIR,"data_batch_1"),'rb') as f:
+    data = cPickle.load(f,encoding='bytes')
+    print(type(data))
+    print(data.keys())
+    print(type(data[b'filenames']))
+    print(type(data[b'data']))
+    print(type(data[b'batch_label']))
+    print(type(data[b'labels']))
+
+    print(data[b'data'].shape)
+    print(data[b'data'][0:2])
+    print(data[b'filenames'][0:2])
+    print(data[b'batch_label'])
+    print(data[b'labels'][0:2])
+#32 * 32 = 1024 * 3(3 aisle) = 3072
+#RR-GG－BB＝３０７２
+#labels:标签
+#>[6 9]分别代表第7类和第10类
+
+
+image_arr = data[b'data'][100]
+image_arr = image_arr.reshape((3, 32, 32)) #3表示3个维度，32 32分别表示图片大小32*32
+image_arr = image_arr.transpose((1, 2,0))#图片通道显示顺序为 32 32 3 所以需要交换通道
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+
+imshow(image_arr)
+plt.show()
+
